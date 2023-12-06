305912189e6ec61be6e642fdd4df580a4c61a501
MateoCarrascoArgano <153085701+MateoCarrascoArgano@users.noreply.github.com>
2023-12-06 12:10:56 -0300

    2nd Commit

--- Argano Projects Page/config/arganoProjectsPage/force-app/main/default/lwc/projectsMainForm/projectsMainForm.js
+++ Argano Projects Page/config/arganoProjectsPage/force-app/main/default/lwc/projectsMainForm/projectsMainForm.js
@@ -1,65 +1,45 @@
 import { LightningElement, track } from 'lwc';
 
-export default class ProjectForm extends LightningElement {
-    @track personName;
-    @track projectName;
-    @track description;
-    @track githubLink;
-    @track imageUrl;
-    @track isSubmitted = false;
+    export default class YourComponent extends LightningElement {
+        @track projectName = '';
+        @track description = '';
+        @track githubLink = '';
+        @track imageUrl = '';
+        @track isSubmitted = false;
 
-    handlePersonNameChange(event) {
-        this.personName = event.target.value;
-    }
+        get isSubmitButtonDisabled() {
+            return !(this.projectName && this.description && this.githubLink && this.imageUrl);
+        }
 
-    handleProjectNameChange(event) {
-        this.projectName = event.target.value;
-    }
+        handleProjectNameChange(event) {
+            this.projectName = event.target.value;
+        }
 
-    handleDescriptionChange(event) {
-        this.description = event.target.value;
-    }
+        handleDescriptionChange(event) {
+            this.description = event.target.value;
+        }
 
-    handleGithubLinkChange(event) {
-        this.githubLink = event.target.value;
-    }
+        handleGithubLinkChange(event) {
+            this.githubLink = event.target.value;
+        }
 
-    handleImageUrlChange(event) {
-        this.imageUrl = event.target.value;
-    }
+        handleImageUrlChange(event) {
+            this.imageUrl = event.target.value;
+        }
 
-    get allFieldsCompleted() {
-        return !(this.personName && this.projectName && this.githubLink);
-    }
+        submitHandler(event) {
+            event.preventDefault();
+            if (this.isSubmitButtonDisabled) {
+                return;
+            }
+            this.isSubmitted = true;
+        }
 
-    submitHandler(event) {
-        event.preventDefault();
-        this.isSubmitted = true;
-        this.saveForm();
+        resetHandler() {
+            this.projectName = '';
+            this.description = '';
+            this.githubLink = '';
+            this.imageUrl = '';
+            this.isSubmitted = false;
+        }
     }
-
-    resetHandler() {
-        this.personName = '';
-        this.projectName = '';
-        this.description = '';
-        this.githubLink = '';
-        this.imageUrl = '';
-        this.isSubmitted = false;
-    }
-
-    saveForm() {
-        saveProject({ 
-            personName: this.personName, 
-            projectName: this.projectName,
-            description: this.description,
-            githubLink: this.githubLink,
-            imageUrl: this.imageUrl,
-        })
-        .then(result => {
-            console.log('You have Submitted your project correctly', result);
-        })
-        .catch(error => {
-            console.error('There was an error', error);
-        });
-    }
-}
